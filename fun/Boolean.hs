14a0b0af245be5cc588be1b82d875fbd16b40f4b
Mateus Sousa <mattahyah@gmail.com>
2022-09-20 11:07:33 -0300

    Aula 4

---
+++ fun/Boolean.hs
@@ -0,0 +1,11 @@
+module Boolean where
+
+data Boolean = T | F deriving(Show)
+
+lor :: Boolean -> Boolean -> Boolean
+lor F F = F
+lor _ _ = T
+
+land :: Boolean -> Boolean -> Boolean
+land T T = T
+land _ _ = F
